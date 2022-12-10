2a71f0bebb6cf031a647cf2b5ef3c1fd50158958
Quwam Ogunlaja <quwam32@gmail.com>
2022-12-10 12:19:09 +0000

    corrected styling errors

--- params.c
+++ params.c
@@ -8,9 +8,9 @@
 params_t *init_params(void)
 {
 	params_t *params = malloc(sizeof(params_t));
-    if (params == NULL)
-		exit(1);
 
+	if (params == NULL)
+		exit(1);
 	params->plus_flag = 0;
 	params->space_flag = 0;
 	params->hash_flag = 0;
